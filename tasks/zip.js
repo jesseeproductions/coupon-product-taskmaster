bf516a69c9ee5fdf12816940a609bd8709763f4e
jesseeproductions <info@jesseeproductions.com>
2023-03-08 13:49:52 -0500

    Partial Fix to Zip tasks

--- tasks/zip.js
+++ tasks/zip.js
@@ -5,49 +5,39 @@ var sync     = require( 'fs-sync' );
 var sequence = require( 'run-sequence' ).use( gulp );
 
 // this task copies files we'll zip into a build directory
-gulp.task( 'zip-copy-files', function() {
-  'use strict';
+gulp.task( 'zip-copy-files', done => {
+    'use strict';
 
-  var json = JSON.parse( fs.readFileSync( './package.json' ) );
-  var zip_include = JSON.parse( fs.readFileSync( './package-whitelist.json' ) );
+	  var json = JSON.parse( fs.readFileSync( './package.json' ) );
+	  var zip_include = JSON.parse( fs.readFileSync( './package-whitelist.json' ) );
 
-	sync.mkdir( json._zipfoldername );
-	return gulp.src( zip_include, { base: '.' } )
-		.pipe( gulp.dest( json._zipfoldername ) );
-} );
-
+		sync.mkdir( json._zipfoldername );
+		return gulp.src( zip_include, { base: '.' } )
+			.pipe( gulp.dest( json._zipfoldername ) );
+});
 // this does the zipping
-gulp.task( 'zip-do-zip', function() {
-  'use strict';
+gulp.task( 'zip-do-zip', done => {
+	'use strict';
 
-  var json = JSON.parse( fs.readFileSync( './package.json' ) );
+	var json = JSON.parse( fs.readFileSync( './package.json' ) );
 
-	return gulp.src( json._zipfoldername + '/**/*', { base: '.' } )
+	return gulp.src( json._zipfoldername + '/**/*', {base: '.'} )
 		.pipe( zip( json._zipname + '.' + json.version + '.zip' ) )
 		.pipe( gulp.dest( '../' ) );
 } );
 
 // this cleans up the trash
-gulp.task( 'zip-purge-build-dir', function( cb ) {
+gulp.task( 'zip-purge-build-dir', done => {
   'use strict';
 
   var json = JSON.parse( fs.readFileSync( './package.json' ) );
 
 	sync.remove( json._zipfoldername );
 
-	cb();
+	done();
 } );
 
-var zip_task = function( cb ) {
-  'use strict';
 
-  sequence(
-    'zip-copy-files',
-    'zip-do-zip',
-    'zip-purge-build-dir',
-    cb
-  );
-};
-
-gulp.task( 'zip', zip_task );
-module.exports = zip_task;
+gulp.task( 'zip', gulp.series( 'zip-copy-files', 'zip-do-zip', 'zip-purge-build-dir', function( done ) {
+	done();
+} ) );
