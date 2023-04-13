9ed1cad66422cd7bbeb25851b7b460300db0a798
jesseeproductions <info@jesseeproductions.com>
2023-04-13 06:24:57 -0400

    Add volt vector sass

--- tasks/sass.js
+++ tasks/sass.js
@@ -10,4 +10,30 @@ var sass_task = function() {
 };
 
 gulp.task( 'sass', sass_task );
-module.exports = sass_task;
+
+var sass_task_volt = function() {
+  'use strict';
+
+  return gulp.src( './Volt_Vectors/resources/scss/**/*.scss' )
+    .pipe( sass().on( 'error', sass.logError ) )
+    .pipe( gulp.dest( './Volt_Vectors/resources/css' ) );
+};
+
+gulp.task( 'sass-volt', sass_task_volt );
+
+
+var sass_task_volt_pro = function() {
+  'use strict';
+
+  return gulp.src( './Volt_Vectors_Pro/resources/scss/**/*.scss' )
+    .pipe( sass().on( 'error', sass.logError ) )
+    .pipe( gulp.dest( './Volt_Vectors_Pro/resources/css' ) );
+};
+
+gulp.task( 'sass-volt-pro', sass_task_volt_pro );
+
+module.exports = {
+	sass_task,
+	sass_task_volt,
+	sass_task_volt_pro
+}
